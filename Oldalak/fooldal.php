1773fa4001c667edca58daa9cab109fb468c8556
Laurinyecz Istvan <laurinyeczistvan@pc0382.eduroam.bibl.u-szeged.hu>
2024-03-13 11:03:22 +0100

    fooldal es hivatkozasok megoldva

--- Oldalak/fooldal.php
+++ Oldalak/fooldal.php
@@ -12,11 +12,11 @@
 		<table>
 			<tr>
 				<td id="kep"><img src="..\Resources\Images\Icons\logo.gif" alt="Logo"></td>
-				<td class="menu-gombok"> <button>Főoldal</button></td>
-				<td class="menu-gombok"> <button class="menu-gombok">Saját aukció</button></td>
-				<td class="menu-gombok"> <button class="menu-gombok">Egyenleg feltöltés</button></td>
-				<td class="menu-gombok"> <button class="menu-gombok">Fórum</button></td>
-				<td class="menu-gombok"> <button class="menu-gombok">Profil</button></td>
+				<td class="menu-gombok"> <a href="fooldal.php">Főoldal</a></td>
+				<td class="menu-gombok"> <a href="sajat_aukcio.php" >Saját aukció</a></td>
+				<td class="menu-gombok"> <a href="egyenleg.php" >Egyenleg feltöltés</a></td>
+				<td class="menu-gombok"> <a href="forum.php" >Fórum</a></td>
+				<td class="menu-gombok"> <a href="profil.php" >Profil</a></td>
 				<td class="menu-gombok"> <button class="menu-gombok" onclick="alert('Ez egy aukciós oldal ahol egy kezdő tőkéböl kiindulva kell meggazdagodnod. Különböző tételekre licitálsz és reméled, hogy nem licitálnak túl. A megszerzett tárgyakat felteheted újabb aukciókra és abból szerzed a profitot. Ha minden pénzed elfogyott ne aggódj, naponta egyszer mehetsz dolgozni. Nem minden aukcióra léphetsz be, komolyabb aukciók komolyabb tételeket rejtenek ahova jelvény kell, ezeket a profilodnál tudod megvenni.')">info</button></td>
 			</tr>
 		</table>
@@ -24,16 +24,12 @@
 	<div class="menusor-spacer">
 	</div>
 	<div class="aukcios-felulet">
-	<div id="aukcio" class="inline">
-	</div>
+		<div id="aukcio" class="inline">
+		</div>
 
-
-	<iframe src="Chat.php" title="Chat Box" id="chat-box" class="inline"></iframe>
-
-
-
-	</div>
-
+		<div class ="inline" id="chat-box">
+			<iframe src="Chat.php" title="Chat Box" id="chat-box" class="inline"></iframe>
+		</div>
 	</div>
 		<table>
 		</table>
