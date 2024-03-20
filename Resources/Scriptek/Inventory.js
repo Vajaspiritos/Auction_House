84128facf308885e85fbd16d3b62bc315bf10023
Laurinyecz Istvan <laurinyeczistvan@Laurinyecz-MacBook-Pro.local>
2024-03-20 08:20:40 +0100

    Profil raktár css

--- Resources/Scriptek/Inventory.js
+++ Resources/Scriptek/Inventory.js
@@ -1,6 +1,6 @@
 if(USER_INVENTORY.length == 0){
 	document.getElementById("info-container").style.visibility="hidden";
-	document.getElementById("items-container").innerHTML="Jellenleg csóró vagy, és SEMMID nincs.";
+	document.getElementById("text").innerHTML="Jellenleg csóró vagy, és SEMMID nincs.";
 	
 }else{
 								//lambda my beloved
@@ -27,14 +27,22 @@ if(USER_INVENTORY.length == 0){
 	
 	
 }
-
+if(document.getElementById("text").innerHTML=="Jellenleg csóró vagy, és SEMMID nincs.") 
+{
+document.getElementById("items-container").style.textAlign = "left";
+document.getElementById("items-container").style.color = "white";
+document.body.style.overflow = "hidden";
+}else 
+{
+	document.body.style.overflow = "scroll";
+}
 
 function DisplayInfo(item){
 	document.getElementById("info_image").src = item['Image_src'];
 	document.getElementById("info-description").innerHTML = item['Description'];
 	document.getElementById("info-name").innerHTML = item['Name'];
-	document.getElementById("info-OG_owner").innerHTML = "Eredeti tulajdonos:<br>"+item['Original_owner'];
-	document.getElementById("info-rarity").innerHTML = "Ritkaság:<br>"+item['Rarity']+"(nagyon nem ritka)";
+	document.getElementById("info-OG_owner").innerHTML = item['Original_owner'];
+	document.getElementById("info-rarity").innerHTML = item['Rarity'];
 	
 	
 	
