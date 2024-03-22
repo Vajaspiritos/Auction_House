ab891cc72571b37bf8116847eb6d793756b45aac
ninjonpotato <cpistihun2@gmail.com>
2024-03-22 16:50:37 +0100

    egyenleg oldal

--- Oldalak/egyenleg.php
+++ Oldalak/egyenleg.php
@@ -3,6 +3,7 @@
 	<title>Egyenleg feltöltés</title>
 			<link rel="stylesheet" href="../Resources/CSS/betuTipusok.css">
 	<link rel="stylesheet" href="../Resources/CSS/navbar.css">
+	<link rel="stylesheet" href="../Resources/CSS/egyenleg.css">
 </head>
 <body>
 	<div class="menusor">
@@ -22,9 +23,73 @@
 		</tr>
 		
 		<div class="inline">
-		
 		</div>
 	</table>
+	</div>
+	<div class="menusor-spacer"></div>
+	<div id="egyenleg-div">
+	<span id="egyenleg">Jelenlegi egyenleged:</span><span class="penz">1000</span>
+	</div>
+	<div id="munka-container">
+		<div id="munka-rendszer" class="inline">
+			<p id="magyarazo-szoveg">Ide jönne egy nagy szöveg mint például: Ez egy olyan munka ahol egy katonai hatseregnek segítesz embereket toborozni. A munka egyszerű és sok pénz nem jár érte. Várnod kell a kiszemeltre és mikor jónak látod a helyzetet elé sétálsz és megkéred hogy csatlakozzon a hadsereghez.</p>
+		</div>
+
+	<div id="img-timer-container" class="inline">
+			<div id="img-container">
+					<img src="../Resources/Images/Munka/munka.jpg"alt="kép a munkáról">
+				</div>
+			<div id="timer-div">
+					<div id="timer-belso-div">
+						<span id="timer-text">Még:<span id="timer">0</span>perc van hátra</span>
+			</div>
+				<button>Munka kezdése</button><br>
+				
+			</div>
+		</div>
+	</div>
+	<div id="tier-text">
+			<a>tier badge áruház</a>
+	</div>
+	<div id="tier-container">
+		
+		<div class="tier-img-container" id="very-low-tier">
+			<img src="../Resources/Images/Munka/munka.jpg" alt="tier kep">
+			<div class="penz-container">
+			<p class="penz" id="very-low">100</p>
+			</div>
+		</div>
+
+		<div class="tier-img-container" id="low-tier">
+			<img src="../Resources/Images/Munka/munka.jpg" alt="tier kep">
+			<div class="penz-container">
+			<p class="penz" id="low">100</p>
+			</div>
+		</div>
+		<div class="tier-img-container" id="medium-tier">
+			<img src="../Resources/Images/Munka/munka.jpg" alt="tier kep">
+			<div class="penz-container">
+			<p class="penz" id="medium">100</p>
+			</div>
+		</div>
+		<div class="tier-img-container" id="high-tier">
+			<img src="../Resources/Images/Munka/munka.jpg" alt="tier kep">
+			<div class="penz-container">
+			<p class="penz" id="high">100</p>
+			</div>
+		</div>
+		<div class="tier-img-container" id="very-high-tier">
+			<img src="../Resources/Images/Munka/munka.jpg" alt="tier kep">
+			<div class="penz-container">
+			<p class="penz" id="very-high">100</p>
+			</div>
+		</div>
+		<div class="tier-img-container" id="ultra-high-tier">
+			<img src="../Resources/Images/Munka/munka.jpg" alt="tier kep">
+			<div class="penz-container">
+			<p class="penz" id="ultra-high">100</p>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
