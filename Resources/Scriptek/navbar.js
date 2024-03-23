b86e7ca60c391b42523b862c0077afea3ecfa635
ninjonpotato <cpistihun2@gmail.com>
2024-03-23 21:15:09 +0100

    navbar animaciok"

--- Resources/Scriptek/navbar.js
+++ Resources/Scriptek/navbar.js
@@ -5,8 +5,10 @@ function lenyit()
 			if(megnyomva== true)
 			{
 				
+				
 				document.getElementById("drop-menusor").classList.remove("active");
 				document.getElementById("drop-menusor").classList.add("inactive");
+				document.getElementsByClassName("inactive")[0].style.animation = "eltunes 1s";
 				document.getElementById("drop-btn").innerHTML= "↓";
 				document.getElementById("drop-btn").style.color = "white";	
 		
@@ -15,8 +17,10 @@ function lenyit()
 
 			}else 
 			{
+				
 				document.getElementById("drop-menusor").classList.add("active");
 				document.getElementById("drop-menusor").classList.remove("inactive");
+				document.getElementsByClassName("active")[0].style.animation = "attones 1s";
 				document.getElementById("drop-btn").innerHTML= "↑";
 				document.getElementById("drop-btn").style.color = "black";
 			}
