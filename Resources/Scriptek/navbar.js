0490c6ea47ed329552e8cdab223f78d01e6c735b
ninjonpotato <cpistihun2@gmail.com>
2024-03-23 20:44:24 +0100

    navbar cucc

--- Resources/Scriptek/navbar.js
+++ Resources/Scriptek/navbar.js
@@ -2,17 +2,21 @@
 var megnyomva = false;
 function lenyit() 
 {
-			if(document.getElementById("drop-menusor").style.display == "block")
+			if(megnyomva== true)
 			{
+				
+				document.getElementById("drop-menusor").classList.remove("active");
+				document.getElementById("drop-menusor").classList.add("inactive");
 				document.getElementById("drop-btn").innerHTML= "↓";
 				document.getElementById("drop-btn").style.color = "white";	
-				document.getElementById("drop-menusor").style.display = "none";
+		
+
 					
 
 			}else 
 			{
-				
-				document.getElementById("drop-menusor").style.display = "block";
+				document.getElementById("drop-menusor").classList.add("active");
+				document.getElementById("drop-menusor").classList.remove("inactive");
 				document.getElementById("drop-btn").innerHTML= "↑";
 				document.getElementById("drop-btn").style.color = "black";
 			}
