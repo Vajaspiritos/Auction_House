1001160f0ecba920095b4f6e5f281292453d4ed8
ninjonpotato <cpistihun2@gmail.com>
2024-03-23 21:27:57 +0100

    navbar nyil

--- Resources/Scriptek/navbar.js
+++ Resources/Scriptek/navbar.js
@@ -1,5 +1,6 @@
 
 var megnyomva = false;
+document.getElementById("drop-btn").innerHTML= "→";
 function lenyit() 
 {
 			if(megnyomva== true)
@@ -9,7 +10,7 @@ function lenyit()
 				document.getElementById("drop-menusor").classList.remove("active");
 				document.getElementById("drop-menusor").classList.add("inactive");
 				document.getElementsByClassName("inactive")[0].style.animation = "eltunes 1s";
-				document.getElementById("drop-btn").innerHTML= "↓";
+				document.getElementById("drop-btn").innerHTML= "→";
 				document.getElementById("drop-btn").style.color = "white";	
 		
 
@@ -21,7 +22,7 @@ function lenyit()
 				document.getElementById("drop-menusor").classList.add("active");
 				document.getElementById("drop-menusor").classList.remove("inactive");
 				document.getElementsByClassName("active")[0].style.animation = "attones 1s";
-				document.getElementById("drop-btn").innerHTML= "↑";
+				document.getElementById("drop-btn").innerHTML= "←";
 				document.getElementById("drop-btn").style.color = "black";
 			}
 megnyomva = !megnyomva;
