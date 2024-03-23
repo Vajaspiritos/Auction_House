e32cd6a799e0030be7719eea2b62c9afb515b22b
ninjonpotato <cpistihun2@gmail.com>
2024-03-23 21:38:51 +0100

    navbar nyil animáció ujra

--- Resources/Scriptek/navbar.js
+++ Resources/Scriptek/navbar.js
@@ -3,14 +3,13 @@ var megnyomva = false;
 document.getElementById("drop-btn").innerHTML= "→";
 function lenyit() 
 {
+	document.getElementById("drop-btn").style.transition= "0.4s";
 			if(megnyomva== true)
-			{
-				
-				
+			{			
 				document.getElementById("drop-menusor").classList.remove("active");
 				document.getElementById("drop-menusor").classList.add("inactive");
 				document.getElementsByClassName("inactive")[0].style.animation = "eltunes 1s";
-					document.getElementById("drop-btn").style.rotate= "0deg";
+				document.getElementById("drop-btn").style.rotate= "0deg";
 				document.getElementById("drop-btn").style.color = "white";	
 		
 
@@ -22,7 +21,7 @@ function lenyit()
 				document.getElementById("drop-menusor").classList.add("active");
 				document.getElementById("drop-menusor").classList.remove("inactive");
 				document.getElementsByClassName("active")[0].style.animation = "attones 1s";
-				document.getElementById("drop-btn").style.rotate= "180deg";
+				document.getElementById("drop-btn").style.rotate= "-180deg";
 				document.getElementById("drop-btn").style.color = "black";
 			}
 megnyomva = !megnyomva;
