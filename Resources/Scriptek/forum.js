8ce00a51ac22025aea75680f650d387928042f14
ninjonpotato <cpistihun2@gmail.com>
2024-03-24 14:09:46 +0100

    forum css alakítás

--- Resources/Scriptek/forum.js
+++ Resources/Scriptek/forum.js
@@ -6,13 +6,13 @@ function legordul()
 		//media query cuccok. ha a képernyő akkora mint a mac-en akkor 
 		document.getElementById("publish").classList.remove("eltunt");
 		document.getElementById("publish").classList.add("megjelent");
-			document.getElementsByClassName("megjelent")[0].style.animation = "attunesDoboz 1s";
+			document.getElementsByClassName("megjelent")[0].style.animation = "attunesDoboz 0.2s";
 	}
 	else 
 	{
 		document.getElementById("publish").classList.add("eltunt");
 		document.getElementById("publish").classList.remove("megjelent");
-			document.getElementsByClassName("eltunt")[0].style.animation = "eltunesDoboz 1s";
+			document.getElementsByClassName("eltunt")[0].style.animation = "eltunesDoboz 0.2s";
 
 	}
 		megnyomva = !megnyomva;
