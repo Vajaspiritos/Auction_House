d13265b851ea03beb546d0dbef6617d450d8e06f
Varga Csanád Sándor <gugla9@gmail.com>
2024-04-21 18:08:18 +0200

    refresh

--- Oldalak/work.php
+++ Oldalak/work.php
@@ -1,17 +1,19 @@
 <?php
 include '../Resources/Scriptek/CheckForLoggedIn.php';
 include '../Resources/Scriptek/ConnectToDB.php';
+
 $res =$conn->query("SELECT LastWork FROM `users` WHERE ID=".$_SESSION["UserID"])->fetch_assoc()["LastWork"];
 if($res ==null){ $res = 3600;
 }else $res = strtotime(date("Y-m-d H:i:s"))-strtotime($res);
 
 if($res >= 3600){
 	UpdateGeneralInfos($conn);
-	$penz = $_SESSION["Money"]+10*(10**($_SESSION["Tier"]));
+	$penz = $_SESSION["Money"]+10*((10**($_SESSION["Tier"]))/4);
 	$date = date("Y-m-d H:i:s");
 	$conn->query("UPDATE `users` SET Money=$penz,LastWork='$date' WHERE ID=".$_SESSION["UserID"]);
 	
 }
 
  header("Location: egyenleg.php");
+ 
 ?>
